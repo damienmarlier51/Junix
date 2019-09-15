b7d5417e8b2a452c027498898004ab0be6730345
--global <damien.marlier@hotmail.fr>
2019-09-15 18:04:34 +0800

    Edit package version in setup

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="junix",
-    version="0.1.0",
+    version="0.1.1",
     author="Damien Marlier",
     author_email="damien.marlier@hotmail.fr",
     description="Simple library to export images from Jupyter notebook",
