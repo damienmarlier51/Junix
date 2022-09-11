6eabd4a20c50121b785c957b2091368474746d11
damienmarlier51 <damien.marlier@hotmail.fr>
2022-09-11 22:26:32 +0800

    Add get_images method (#23)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="junix",
-    version="0.1.4",
+    version="0.1.5",
     author="Damien Marlier",
     author_email="damien.marlier@hotmail.fr",
     description="Utils to export images from Jupyter notebook",
