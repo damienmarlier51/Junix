739d197880a65f2e7c4878bb6d3eb44057950ff4
damienmarlier51 <damien.marlier@hotmail.fr>
2022-09-11 11:30:23 +0800

    Improve unit test

--- example/export_example.py
+++ example/export_example.py
@@ -1,6 +1,14 @@
+import json
+
 import junix
 
 if __name__ == "__main__":
+
+    with open("example/example.ipynb", "r") as f:
+        notebook_dict = json.load(f)
+
+    with open("example/example.json", "w") as f:
+        json.dump(notebook_dict, f)
 
     junix.export_images(
         filepath="example/example.ipynb",
